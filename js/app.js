1ac3f7a88374b6bac9ef1df98ec598e06996ad51
“mariaahvgit config --global user.name “mariaahv <mariahv14>
2018-01-01 02:22:52 -0500

    añadiendo mouseover

--- js/app.js
+++ js/app.js
@@ -1,7 +1,50 @@
 $(document).ready(function(){
-$('.gmaps').gmaps();
-});
+    $('.gmaps').gmaps();
+    $("#img1").mouseover(function(){
+        $("#img1 ").css("width", "50%");
+    });
+    $('#img1').mouseout(function()
+         {
+      $("#img1").css("width", "28%");
+    });
 
-$(".images").mouseover(function(){
-    $(".images").css("width", "100%");
+    $("#img2").mouseover(function(){
+        $("#img2 ").css("width", "50%");
+    });
+    $('#img2').mouseout(function()
+         {
+      $("#img2").css("width", "28%");
+    });
+    $("#img3").mouseover(function(){
+        $("#img3 ").css("width", "50%");
+    });
+    $('#img3').mouseout(function()
+         {
+      $("#img3").css("width", "28%");
+    });
+
+    $("#img4").mouseover(function(){
+        $("#img4").css("width", "50%");
+    });
+    $('#img4').mouseout(function()
+         {
+      $("#img4").css("width", "28%");
+    });
+
+    $("#img5").mouseover(function(){
+        $("#img5").css("width", "50%");
+    });
+    $('#img5').mouseout(function()
+         {
+      $("#img5").css("width", "28%");
+    });
+
+    $("#img6").mouseover(function(){
+        $("#img6").css("width", "50%");
+    });
+    $('#img6').mouseout(function()
+         {
+      $("#img6").css("width", "28%");
+    });
+
 });
