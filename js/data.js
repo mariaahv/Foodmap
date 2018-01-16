1e21cd7d264dbc8c2be966f257f6d0f113ce5c1d
“mariaahv <mariahv14>
2018-01-15 22:49:25 -0500

    añadiendo data

---
+++ js/data.js
@@ -0,0 +1,43 @@
+var data=[{
+  'name':'astrid y gaston',
+  'image':'assets/images/food.jpg',
+  'direccion':'miraflores 2052',
+  'precio regular':'$50',
+  'tipo':'comida criolla',
+
+},
+{
+  'name':'panchita',
+  'image':'assets/images/food1.jpg',
+  'direccion':'los olivos las gardenias',
+  'precio regular':'$20',
+  'tipo':'anticucheria',
+},
+{
+  'name':'chicken family',
+  'image':'assets/images/food2.jpg',
+  'direccion':'puente piedra',
+  'precio regular':'$30',
+  'tipo':'polleria',
+},
+{
+  'name':'punto azul',
+  'image':'assets/images/food3.jpg',
+  'direccion':'la mar 5046',
+  'precio regular':'$80',
+  'tipo':'pescados y mariscos',
+},
+{
+  'name':'china food',
+  'image':'assets/images/food4.jpg',
+  'direccion':'',
+  'precio regular':'$60',
+  'tipo':'comida fusion',
+},
+{
+  'name':'telepizza',
+  'image':'assets/images/food5.jpg',
+  'direccion':'independencia los rosales',
+  'precio regular':'$50',
+  'tipo':'pizzeria',
+}];
